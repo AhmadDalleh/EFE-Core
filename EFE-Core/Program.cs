6c7a71e4e4df84f8f5a705bc85604ecf6253d679
m.jabban <m.jabban@TESTER>
2025-02-27 10:48:40 +0300

    Add project files.

---
+++ EFE-Core/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using EFE_Core;
+using static EFE_Core.ApplicationDbContext;
+using System.Linq;
+
+
+SeedData();
+static void SeedData()
+{
+    using var context = new ApplicationDbContext();
+
+    //context.Database.EnsureCreated();
+
+    //var blog = context.Blogs.FirstOrDefault(b => b.Url == "www.google.com");
+
+    //if(blog == null)
+    //    context.Blogs.Add(new Blog { Url = "www.google.com" });
+
+    context.SaveChanges();
+    
+}
